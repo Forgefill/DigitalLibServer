9a07d0d854dfdf78e092a85ecb7cbb421e8f0f14
bubkamisha14 <bubkamisha14@gmail.com>
2023-02-15 22:40:30 +0200

    add jwt token authentication

---
+++ DigitalLibServer/JWTAuth/AuthOptions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace DigitalLibServer.JWTAuth
+{
+    public class AuthOptions
+    {
+        public static string Key { get; } = "UhVWKa0vmBjUcfAwHWar";
+
+        public static string ValidIssuer { get; } = null;
+
+        public static string ValidAudience { get; } = null;
+
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key));
+        }
+
+    }
+}
